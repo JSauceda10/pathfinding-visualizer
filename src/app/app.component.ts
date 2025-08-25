8ff7fdeef6797d2d85a0c035e75c0aa8b206f31a
JSauceda10 <j.sauceda10@yahoo.com>
2025-08-25 16:55:02 -0500

    Initial setup for the pathfinding visualizer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BoardComponent } from './components/board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [NavbarComponent, BoardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
