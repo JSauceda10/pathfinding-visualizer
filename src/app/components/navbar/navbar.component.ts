6ded9ab1cd52a868032f4ef40412a5149ed1ba7f
JSauceda10 <j.sauceda10@yahoo.com>
2025-08-25 20:18:59 -0500

    styling the navbar and its options

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faEraser, faPlay, faRedo, faRedoAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  faPlay = faPlay;
+  faRedoAlt = faRedoAlt;
+  faEraser = faEraser
 }
